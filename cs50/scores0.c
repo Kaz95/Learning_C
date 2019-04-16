b5efb2f2a6f399ad2bb0a9d452a130f04cb16fee
Kazact <kazact95@gmail.com>
2019-04-16 09:46:41 -0500

    scores.c example fixt

--- cs50/scores0.c
+++ cs50/scores0.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+void chart(int score);
+
+void main(void)
 {
-	char score1[101];
-	puts("Score 1: ");
-	fgets(score1, 100, stdin);
-	int int_score = atoi(score1);
+	char score1[4];
+	char score2[4];
+	char score3[4];
+	printf("Score 1: ");
+	fgets(score1, 5, stdin);
+
+	printf("Score 2: ");
+	fgets(score2, 5, stdin);
+
+	printf("Score 3: ");
+	fgets(score3, 5, stdin);
+
+	int int_score1 = atoi(score1);
+	int int_score2 = atoi(score2);
+	int int_score3 = atoi(score3);
 
 	printf("Score 1: ");
-	for (int i = 0; i < int_score; i++)
+	chart(int_score1);
+
+	printf("Score 2: ");
+	chart(int_score2);
+
+	printf("Score 3: ");
+	chart(int_score3);
+}
+
+void chart(int score)
+{
+	for(int i = 0; i < score; i++)
 	{
 		printf("#");
 	}
 	printf("\n");
-
-	printf("Score 2: ");
-	printf("Score 3: ");
 }
-
